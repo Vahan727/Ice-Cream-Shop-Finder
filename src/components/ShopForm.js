aec1b71366afb402c34e1b2906b1c3419c257d43
Adeyemi Arimoro <arimoro71@gmail.com>
2023-05-16 14:30:48 -0500

    Add cost, flavor, description inpus

--- src/components/ShopForm.js
+++ src/components/ShopForm.js
@@ -5,12 +5,15 @@ function ShopForm(){
 
 
     return (
-        <div className="new-plant-form">
-          <h2>New Plant</h2>
+        <div className="new-shop-form">
+          <h2>New Shop</h2>
           <form >
             <input type="text" name="name" placeholder="Shop name" />
-            <input type="text" name="location" placeholder="Shop location" />
             <input type="text" name="image" placeholder="Image URL" />
+            <input type="text" name="flavor image" placeholder="Flavor image" />
+            <input type="text" name="description" placeholder="Flavor description" />
+            <input type="number" name="cost" step="1.0" placeholder="Cost" />
+            <input type="text" name="location" placeholder="Shop location" />
             <input type="number" name="rating" step="1.0" placeholder="Shop Rating" />
             <button type="submit">Add Shop</button>
           </form>
